42c5e6e7d0d8e24808c72e43eff33ef084e242d3
min <smh92333@gmail.com>
2022-10-21 15:01:40 +0900

    reviews/delete

--- reviews/urls.py
+++ reviews/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("create/", views.create, name="create"),
     path("<int:review_pk>/", views.detail, name="detail"),
     path("<int:review_pk>/update", views.update, name="update"),
+    path("<int:review_pk>/delete", views.delete, name="delete"),
 
 ]
