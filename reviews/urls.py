c3176baf7e2f9070019dc4a3c10f12eb3b2d230f
w00ye0l <dnduf158@gmail.com>
2022-10-21 14:30:16 +0900

    reviews/detail

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,5 +6,5 @@ app_name = "reviews"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
-
+    path("<int:review_pk>/", views.detail, name="detail"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,30 +1,37 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import ReviewForm
 from .models import Review
+
 # Create your views here.
+
 
 def index(request):
     reviews = Review.objects.all()
-    context = {
-        'reviews': reviews
-    }
+    context = {"reviews": reviews}
 
-    return render(request, 'reviews/index.html', context)
+    return render(request, "reviews/index.html", context)
+
 
 def create(request):
-    if request.method =='POST':
+    if request.method == "POST":
         review_form = ReviewForm(request.POST)
         if review_form.is_valid():
             form = review_form.save(commit=False)
             form.user = request.user
             form.save()
-            return redirect('reviews:index')
+            return redirect("reviews:index")
 
-    else:   
+    else:
         review_form = ReviewForm()
 
+    context = {"review_form": review_form}
+
+    return render(request, "reviews/create.html", context)
+
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
     context = {
-        'review_form' : review_form
+        "review": review,
     }
-    
-    return render(request,'reviews/create.html',context)
+    return render(request, "reviews/detail.html", context)
